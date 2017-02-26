cdbbe91bc3333728b5b7fd42f69cddf87ed5c722
root <gomis103@gmail.com>
2017-02-26 18:51:24 +0100

    entities

---
+++ src/AppBundle/Entity/Post.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Post
+ *
+ * @ORM\Table(name="post")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\PostRepository")
+ */
+class Post
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Something", type="string", length=255)
+     */
+    private $something;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Post
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set something
+     *
+     * @param string $something
+     *
+     * @return Post
+     */
+    public function setSomething($something)
+    {
+        $this->something = $something;
+
+        return $this;
+    }
+
+    /**
+     * Get something
+     *
+     * @return string
+     */
+    public function getSomething()
+    {
+        return $this->something;
+    }
+}
+
