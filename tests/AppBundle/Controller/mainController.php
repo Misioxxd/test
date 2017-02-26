b6ca5bcffa67e7678b10dd36dc37cf390dffa970
GM <gomis103@gmail.com>
2017-02-26 18:32:57 +0100

    prepare

--- tests/AppBundle/Controller/mainController.php
+++ tests/AppBundle/Controller/mainController.php
@@ -1,7 +1,20 @@
 <?php
+namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
-class mainController
+class mainController extends Controller
 {
 
+    /**
+     * @Route ("/")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction(Request $request)
+    {
+        return $this->render(":default:index.html.twig");
+    }
 }
