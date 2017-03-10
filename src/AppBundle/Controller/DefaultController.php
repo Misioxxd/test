a2a6cb2bf6cc44abc9a986e879b3e465df14219e
Michalek <gomis103@gmail.com>
2017-03-10 16:14:45 +0100

    add delete button,change styles,add flashMessages

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,8 +4,11 @@ namespace AppBundle\Controller;
 
 use AppBundle\Form\Type\ProgrammerType;
 use AppBundle\Form\Type\RelationType;
+use Doctrine\DBAL\Types\TextType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\BrowserKit\Response;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -17,7 +20,7 @@ class DefaultController extends Controller
      * @Route("/b", name="InsertData")
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function anotherAction(Request $request)
+    public function addAction(Request $request)
     {
 
 
@@ -32,7 +35,10 @@ class DefaultController extends Controller
             $product = $form->getData();
             $em->persist($product);
             $em->flush();
-            return $this->redirect($request->getUri());
+            $this->get('session')->getFlashBag()->add('success', 'Dodano pracownika');
+            $url = $this->generateUrl('showData');
+
+            return $this->redirect($url);
 
         }
         return $this->render('default/add.html.twig',[
@@ -59,6 +65,7 @@ class DefaultController extends Controller
         return $this->render(':default:show.html.twig',[
             'data'=>$showData
         ]);
+
     }
 
 
@@ -72,12 +79,49 @@ class DefaultController extends Controller
     {
         return $this->render(':default:main.html.twig');
     }
-    /**
-     *
-     * @Route ("/edit/{id}", name="editPage")
-     * **/
-//    public function editAction($id) {
-//
-//    }
 
+    /**
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route ("/edit/{id}", name="Edit")
+     */
+    public function editAction($id,Request $request) {
+        $edit =$this->getDoctrine()->getManager()->getRepository('AppBundle:Test')
+            ->findOneBy(array('id'=>$id));
+        $form = $this->createForm(RelationType::class,$edit);
+        $form ->add('save',SubmitType::class,array(
+            'label'=>"wyslij "
+        ));
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
+            $product = $form->getData();
+            $em->persist($product);
+            $em->flush();
+            $this->get("session")->getFlashBag()->add("success","Edycja została ukończona pomyślnie");
+
+            return $this->redirect('/a');
+        }
+        return $this->render('default/add.html.twig',[
+            'form1'=>$form->createView()
+        ]);
+
+    }
+
+    /**
+     * @Route ("/d/{id}", name="Delete")
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function deleteAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $adminentities = $em->getRepository('AppBundle:Test')->find($id);
+        $em->remove($adminentities);
+        $em->flush();
+        $this->get('session')->getFlashBag()->add('success','Usuwanie przebiegło pomyślnie');
+        return $this->redirect("/a");
+    }
 }
